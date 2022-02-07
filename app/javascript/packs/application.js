a99a6187dc4fcf3458150a22951da84476e40f8f
naonao-program <gf19a012@oecu.jp>
2022-02-07 13:47:45 +0900

    top-sidebar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,11 @@ require("channels")
 import 'bootstrap';
 import '../stylesheets/application';
 import '@fortawesome/fontawesome-free/js/all';
+
+//= require jquery3
+//= require popper
+//= require bootstrap
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
