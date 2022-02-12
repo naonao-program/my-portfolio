4de9552afdc244df3985ea96f342bc362f82d9ee
naonao-program <gf19a012@oecu.jp>
2022-02-13 00:55:32 +0900

    ボタン生成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
+  resources :blogs, only:[:index]
+  get "camera-portfolio" => "cameras#index"
+  get "programming-portfolio" => "programming#index"
 end
