fc155b210c40c4f64b7645d26e4c2f6cf48a30c5
naonao-program <gf19a012@oecu.jp>
2022-02-26 16:31:20 +0900

    aboutページのパスを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
+  get "about" => "homes#about"
   resources :blogs, only:[:index]
   get "camera-portfolio" => "cameras#index"
   get "programming-portfolio" => "programming#index"
